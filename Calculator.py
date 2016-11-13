29c3e9c545bc4bee665588f970bab74944b1b84a
LinaSerry <linaserry@cmail.carleton.ca>
2016-11-13 11:55:44 -0500

    added a comment

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,5 @@
 ''' Program make a simple calculator that can add, subtract, multiply and divide using functions '''
-
+#Calc
 # define functions
 def add(x, y):
    """This function adds two numbers"""
