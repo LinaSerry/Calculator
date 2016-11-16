9c7849cada5d2bdf376b993cff7f2b36fcc49d70
Lina Serry <lina.serry@ericsson.com>
2016-11-16 13:29:39 -0500

    testing pushing

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,7 @@
 ''' Program make a simple calculator that can add, subtract, multiply and divide using functions '''
 #Calc
 # define functions
+#testing
 def add(x, y):
    """This function adds two numbers"""
    return x + y
